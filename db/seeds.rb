9a11cc415976033633723d2799ae9af203745296
Bruno Pinheiro <bspinheiro@me.com>
2017-02-16 09:02:52 -0200

    Working app

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.create(name:"Anibal", address:"Ipanema", phone_number:"3333 4444", category:"italian")
+Restaurant.create(name:"Oscar", address:"Leblon", phone_number:"3333 4445", category:"french")
+Restaurant.create(name:"Astor", address:"Ipanema", phone_number:"3333 4446", category:"italian")
+Restaurant.create(name:"Gero", address:"Ipanema", phone_number:"3333 4447", category:"italian")
+Restaurant.create(name:"Anna", address:"Lagoa", phone_number:"3333 4448", category:"italian")
